3e6d48e285bd61ed928cdc258294c8f71158c3e7
Akshay Nile <akshaynile8849@gmail.com>
2022-10-01 18:29:56 +0530

    code coverage raised to 93%

--- src/app/components/multiselect/multiselect.component.spec.ts
+++ src/app/components/multiselect/multiselect.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 
 import { MultiselectComponent } from './multiselect.component';
 
@@ -8,7 +9,8 @@ describe('MultiselectComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ MultiselectComponent ]
+      declarations: [ MultiselectComponent ],
+      providers: [FormBuilder]
     })
     .compileComponents();
   });
@@ -21,5 +23,23 @@ describe('MultiselectComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should getAvailableAlphabets for the given index', () => {
+    const comp = spyOn(component, 'getAvailableAlphabets').and.callThrough();
+    component.getAvailableAlphabets(0);
+    expect(comp).toHaveBeenCalled();
+  });
+
+  it('should addNewDropdown', () => {
+    const comp = spyOn(component, 'addNewDropdown').and.callThrough();
+    component.addNewDropdown();
+    expect(comp).toHaveBeenCalled();
+  });
+
+  it('should removeDropdownAt for the given index', () => {
+    const comp = spyOn(component, 'removeDropdownAt').and.callThrough();
+    component.removeDropdownAt(0);
+    expect(comp).toHaveBeenCalled();
   });
 });
