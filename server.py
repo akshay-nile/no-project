78514848501c1c76f6b91b410f358b8f8cae0004
Akshay Nile <akshaynile8849@gmail.com>
2022-11-30 18:51:17 +0530

    updated mimetype detection logic at server

--- server.py
+++ server.py
@@ -1,11 +1,10 @@
 import os
+from mimetypes import guess_type
 from flask import Flask, Response, render_template, request
 
 app = Flask(__name__)
 
-BUILD_PATH = '..\\dist\\no-project'
-MIME_TYPES = {'html': 'html', 'css': 'css', 'js': 'javascript', 'svg': 'svg', 'txt': 'text'}
-
+BUILD_PATH = 'dist\\no-project'
 
 @app.route('/no-project', methods=['GET','POST'])
 @app.route('/no-project/')
@@ -18,11 +17,9 @@ def no_project(path = 'index.html'):
             path = 'index.html'
             filepath = os.path.join(BUILD_PATH, path)
 
-        extension = path.split('.')[-1].strip().lower()
-        mimetype = MIME_TYPES.get(extension)
-
-        filemode, mimetype = ('rb', '*/*') if mimetype is None else ('r', f'text/{mimetype}')
-        return Response(open(filepath, filemode).read(), mimetype=mimetype)
+        content = open(filepath, 'rb').read()
+        mimetype = guess_type(filepath, strict=True)[0]
+        return Response(response=content, mimetype=mimetype, status=200)
     else:
         if request.headers.get('Password') != r'${{secrets.PASSWORD}}':
             return 'unauthentic'
