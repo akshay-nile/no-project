ecf605c8a1739ebffe0f730474896d3cf17b3266
Akshay Nile <akshaynile8849@gmail.com>
2023-07-01 08:56:32 +0530

    added playback capability for spoken lines

--- src/app/components/voice-recog-frontend/voice-recog-frontend.component.ts
+++ src/app/components/voice-recog-frontend/voice-recog-frontend.component.ts
@@ -1,7 +1,9 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { ChangeDetectorRef, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 const SpeechRecognition = (window as any).SpeechRecognition || (window as any).webkitSpeechRecognition;
+
 type SpeechRecognitionStatus = 'STOPPED' | 'LISTENING' | 'PROCESSING';
+type Transcript = { text: string, lang: string };
 
 @Component({
   selector: 'app-voice-recognition-in-frontend',
@@ -12,9 +14,12 @@ export class VoiceRecogFrontendComponent implements OnInit {
   @ViewChild('mic') mic!: ElementRef;
 
   readonly speechRecognition = new SpeechRecognition();
+  readonly speakData = new SpeechSynthesisUtterance();
 
-  lines: string[] = [];
+  lines: Transcript[] = [];
   status: SpeechRecognitionStatus = 'STOPPED';
+
+  playingIndex = -1;
 
   languages = [
     { lang: 'English', code: 'en' },
@@ -25,10 +30,11 @@ export class VoiceRecogFrontendComponent implements OnInit {
     { lang: 'Urdu', code: 'ur' }
   ];
 
-  constructor() { }
+  constructor(private changeDetectorRef: ChangeDetectorRef) { }
 
   ngOnInit(): void {
     this.initSpeechRecognition();
+    this.initSpeechSynthesis();
   }
 
   initSpeechRecognition(): void {
@@ -38,14 +44,23 @@ export class VoiceRecogFrontendComponent implements OnInit {
       const lastIndex = event?.results.length - 1;
       const final = event?.results[lastIndex]?.isFinal;
       const transcript = event?.results[lastIndex][0]?.transcript;
-      if (final) { this.lines[this.lines.length - 1] += transcript; }
+      if (final) { this.lines[this.lines.length - 1].text += transcript; }
       if (!this.status) { this.status = 'STOPPED'; }
+    };
+  }
+
+  initSpeechSynthesis(): void {
+    this.speakData.rate = 0.9; // From 0 to 2
+    this.speakData.pitch = 0.7; // From 0.1 to 10
+    this.speakData.onend = () => {
+      this.playingIndex = -1;
+      this.changeDetectorRef.detectChanges(); // to refresh UI for variable value changes
     };
   }
 
   startListening(): void {
     if (this.status !== 'STOPPED') { return; }
-    this.lines.push('');
+    this.lines.push({ text: '', lang: this.speechRecognition.lang });
     this.speechRecognition.start();
     this.status = 'LISTENING';
   }
@@ -56,17 +71,21 @@ export class VoiceRecogFrontendComponent implements OnInit {
     setTimeout(() => {
       this.speechRecognition.stop();
       this.status = 'STOPPED';
-      this.speak(this.lines[this.lines.length - 1]); // speak-out the transcripted text
+      this.startSpeaking(this.lines.length - 1); // speak-out the transcripted text
     }, 2000);
   }
 
-  speak(text: string): void {
-    let speakData = new SpeechSynthesisUtterance();
-    speakData.rate = 0.9; // From 0 to 2
-    speakData.pitch = 0.7; // From 0.1 to 10
-    speakData.lang = this.speechRecognition.lang + '-IN';
-    speakData.text = text;
-    speechSynthesis.speak(speakData);
-  }
+  startSpeaking(i: number): void {
+    if (this.playingIndex !== -1) { this.stopSpeaking(); } // to stop if already playing
+    this.speakData.lang = this.lines[i].lang + '-IN';
+    this.speakData.text = this.lines[i].text;
+    this.playingIndex = i;  // to change the play-button icon while playing
+    speechSynthesis.speak(this.speakData);
+  };
 
+  stopSpeaking(): void {
+    this.playingIndex = -1;
+    speechSynthesis.cancel();
+  }
 }
+
